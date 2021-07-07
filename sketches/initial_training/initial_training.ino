0d8e81d8e65dc1e9bbf81a203f46138503c26b52
jcslack <33235442+jcslack@users.noreply.github.com>
2021-07-07 17:59:02 -0400

    Update initial_training.ino

--- sketches/initial_training/initial_training.ino
+++ sketches/initial_training/initial_training.ino
@@ -1,19 +1,19 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,20,4);
 
-const int solL = 7;
-const int solR = 6;
-const int senL = 3;
-const int senR = 2;
-const int toneS = 5;
+const int solL = 7; //left solenoid (output)
+const int solR = 6; //right solenoid (output)
+const int senL = 3; //left sensor (input)
+const int senR = 2; //right sensor (input)
+const int toneS = 5; //3.5kHz tone (output)
 const int toneL = 4;
-const int light = 4;
-int senStateL = 0;
+const int light = 4; //houselight (output)
+int senStateL = 0;  //initialize sensor states (high or low)
 int senStateR = 0;
-unsigned long startSession;
-unsigned long startWait;
-const unsigned long runTime = 3600000;
-int unresponsive = 0;
+unsigned long startSession; //initialize zero point for session
+unsigned long startWait;  //initialize zero point for trial
+const unsigned long runTime = 3600000; //length of session (msec)
+int unresponsive = 0; //initialize check for non-response trial
 int U = 0; //number of non-responses
 int R = 0; //number of right port responses
 int L = 0; //number of left port responses
@@ -21,6 +21,7 @@ int T = 0; //total responses (L+R)
 int N = 0; //total trials (L+R+U)
 
 //-----------------functions-----------------------------
+//intializes pin modes and states
 void setPINS(){
   pinMode(solL,OUTPUT);
   pinMode(solR,OUTPUT);
@@ -35,6 +36,7 @@ void setPINS(){
   delay(2500);
 }
 
+//initializes LCD display
 void setLCD(){
   lcd.init();
   lcd.clear();
@@ -44,10 +46,12 @@ void setLCD(){
  
 }
 
+//when called, displays trial counters on LCD screen given N (numTrials), 
+//T (numResponses), and U (numNonresponses) inputs
 void dispTrialCounters(int numTrials,int numResponses, int numNonresponses){
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("---Trial Counters---"
+  lcd.print("---Trial Counters---");
   lcd.setCursor(0,1);
   lcd.print("Non-Responses: ");
   lcd.setCursor(15,1);
@@ -60,14 +64,33 @@ void dispTrialCounters(int numTrials,int numResponses, int numNonresponses){
   lcd.print("Total Trials: ");
   lcd.setCursor(15,3);
   lcd.print(numTrials);
-  delay(6000);
+  delay(4000);
 }
 
+//when called (i.e. when session time reaches runTime), 
+//initiates empty endless while loop  
 void endSession(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Session Time Reached");
+  while(1){
+  }
+}
 
-
+//displays elapsed time on LCD since startSession was set
+void dispElapsedTime(unsigned long t0){
+  lcd.setCursor(0,1);
+  lcd.print("Elapsed Time: ");
+  lcd.setCursor(14,1);
+  lcd.autoscroll();
+  float t;
+  t = (millis()-t0)/(60*1000);
+  lcd.print(t);
+  lcd.print(" min");
+  lcd.noAutoscroll();
   
 }
+
 //------------------------------MAIN------------------------------
 void setup() {
   Serial.begin(9600);
@@ -92,7 +115,7 @@ void loop() {
 //------------------------WAIT FOR SENSOR INPUT--------------
   startWait=millis();
   lcd.setCursor(0,0);
-  lcd.print("Wait for Response");
+  lcd.print("Waiting for Response");
   while(digitalRead(senL) == HIGH && digitalRead(senR) == HIGH){
     if(millis()-startWait >= 20000){
       unresponsive = 1;
@@ -106,37 +129,45 @@ void loop() {
     lcd.setCursor(2,0);
     lcd.print("No Response");
     digitalWrite(toneS,LOW);
-    delay(250);
+    delay(400);
     digitalWrite(toneS,HIGH);
     unresponsive = 0;
+    U++;
   }
   else if(senStateL == LOW){
     lcd.clear();
     lcd.setCursor(2,0);
     lcd.print("Left Port Poke");
-    digitalWrite(toneS,LOW);
-    delay(100);
-    digitalWrite(toneS,HIGH);
     digitalWrite(solL,LOW);
     delay(10);
     digitalWrite(solL,HIGH);
+    digitalWrite(toneS,LOW);
+    delay(100);
+    digitalWrite(toneS,HIGH);
+    L++;
   }
   else if(senStateR == LOW){
     lcd.clear();
     lcd.setCursor(2,0);
     lcd.print("Right Port Poke");
-    digitalWrite(toneS,LOW);
-    delay(100);
-    digitalWrite(toneS,HIGH);
     digitalWrite(solR,LOW);
     delay(10);
     digitalWrite(solR,HIGH);
+    digitalWrite(toneS,LOW);
+    delay(100);
+    digitalWrite(toneS,HIGH);
+    R++;
   }
-    
-    delay(2000);
-    digitalWrite(light,HIGH);
-    lcd.clear();
-    lcd.setCursor(2,0);
-    lcd.print("Next Trial");
-    delay(2000);
+  T=L+R;
+  N=L+R+U;
+  digitalWrite(light,HIGH);
+  dispTrialCounters(N,T,U);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  dispElapsedTime(startSession);
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("-----Next Trial-----");
+  delay(1000);
 }
